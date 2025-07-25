c4518306de911aade11bfff8bdc1712d8fd49fae
Aaron Carr <ad09carr@gmail.com>
2025-07-24 23:23:38 -0400

    Added header, footer, and pages

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script>
+    import "$lib/style.css"
+	import Footer from "$lib/Footer.svelte";
+    import Header from "$lib/Header.svelte";
+</script>
+
+<dif class="Wrapper">
+
+    <Header />
+    
+    <main>
+        <slot></slot>
+    </main>
+    <Footer />
+</dif>
+
+<style>
+    .Wrapper {
+        min-height: 100dvh;
+        display: grid;
+        grid-template-rows: auto 1fr auto;
+    }
+</style>
