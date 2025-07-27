41e48b56c514a0064d7f56ae49e2ae3d5014209d
Aaron Carr <ad09carr@gmail.com>
2025-07-27 14:02:13 -0400

    Added Hero

---
+++ src/lib/Hero.svelte
@@ -0,0 +1,29 @@
+<section>
+    <div class="outer">
+        <div class="inner">
+            <div class="hero-content bg-gradient-blue white flow">
+                <h1>This is a template Page</h1>
+                <p class="white">This will be a paragraph, edit this to be a paragraph when using this template.</p>
+                <a href="/" class="button bg-gradient-pink white">Get Started</a>
+            </div>
+        </div>
+    </div>
+</section>
+
+<style>
+    .outer {
+        background-image: url(https://picsum.photos/1600/600/);
+        background-size: cover;
+        background-position: center;
+    }
+
+    .inner {
+        padding-block: 8rem;
+    }
+
+    .hero-content {
+        max-width: 32rem;
+        padding: 3rem;
+        border-radius: 1rem;
+    }
+</style>
