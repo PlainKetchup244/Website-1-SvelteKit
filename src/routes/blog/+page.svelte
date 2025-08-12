3d9a27af4e398c1dc734cfc544babe628d5a9107
Aaron Carr <ad09carr@gmail.com>
2025-08-12 15:12:16 -0400

    Added dynamic blog pages

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -4,4 +4,17 @@
 export let data
 </script>
 
-<Title name="Blog" />\
+<Title name="Blog" />
+
+<section>
+	<div class="outer">
+		<div class="inner auto-fit">
+			{#each data.posts as post}
+			<div class = "flow">
+				<h2><a href="/blog/{post.id}">{post.title}</a></h2>
+				<p>{post.body}</p>
+			</div>
+			{/each}
+		</div>
+	</div>
+</section>
